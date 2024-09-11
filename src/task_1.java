4446d0b76007467e8c708b50b2b49746f651c679
BoHdANchIck <daneliuk.bohdan@chnu.edu.ua>
2024-09-11 20:06:45 +0300

    1_task.1

--- src/task_1.java
+++ src/task_1.java
@@ -1,7 +1,7 @@
 import MyClasses.Account;
 import java.util.Scanner;
 
-public class Main {
+public class task_1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
